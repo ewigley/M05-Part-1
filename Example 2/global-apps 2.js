c579b5904f73e0e8004697c0996186e8dd518eaa
Evan Wigley <pianoshredder89@gmail.com>
2023-12-03 14:58:10 -0500

    M05 Part 1

---
+++ Example 2/global-apps 2.js
@@ -0,0 +1,26 @@
+import Element from "./element 2.js";
+const GlobalApp = {
+  data() {
+    return {
+      elements : []
+    }
+  },
+  components : {
+    Element:Element
+  },
+  template : `
+    <button @click="add()">Add Element</button>
+    <ul>
+      <li v-for="(element, index) in elements" 
+      :key="index">{{element}}</li>
+    </ul>
+  `,
+  methods : {
+    add() {
+      var element = "Element " + (this.elements.length + 
+      1);  // "Element X"
+      this.elements.push(element);
+    }
+  }
+}
+export default GlobalApp;
